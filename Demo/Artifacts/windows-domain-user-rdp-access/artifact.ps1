29a862b749ccc12a5e8956c56b49e1d75b7e9749
peakchallenge <Gary_Dutton@hotmail.com>
2019-01-23 22:10:49 +0000

    Updated
    Updated

--- Demo/Artifacts/windows-domain-user-rdp-access/artifact.ps1
+++ Demo/Artifacts/windows-domain-user-rdp-access/artifact.ps1
@@ -82,7 +82,8 @@ try
     foreach ($allowRDPUser in $allowRDP){
         Add-Content -Path $file -Value "Attempting to add Users to RDP Group - $allowRDPUser"
         #Add-LocalGroupMember -Group "Remote Desktop Users" -Member "$allowRDPUser"
-        NET LOCALGROUP "Remote Desktop Users" $allowRDPUser /ADD
+        write-host "$allowRDPUser"
+        NET LOCALGROUP "Remote Desktop Users" "$allowRDPUser" /ADD
     }
 
     Add-Content -Path $file -Value "Entering Main Script Block (try) - Artifact Applied Successfully"
