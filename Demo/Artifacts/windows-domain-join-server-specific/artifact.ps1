202ddd96cacb6a49040e7cdcd5dd1b3883dcaf73
peakchallenge <Gary_Dutton@hotmail.com>
2019-06-05 13:36:23 +0100

    Added delays for quicker workstations!
    Added several sleep statements to slow things down on quicker VMs.

--- Demo/Artifacts/windows-domain-join-server-specific/artifact.ps1
+++ Demo/Artifacts/windows-domain-join-server-specific/artifact.ps1
@@ -24,10 +24,13 @@ if(!(Test-Path -Path $logfilePATH )){
 
 # Create the log file, make sure c:\temp already exists
 $file = Join-Path -Path (Resolve-Path "c:\Temp\") -ChildPath "artifactlog.txt"
+sleep 10
 New-Item $file -type file | Out-Null
+sleep 10
 
 # Add content to the log file, can add these debugging statements throughout your powershell to identify which line is hanging
 Add-Content -Path $file -Value "Beginning Script Process - starting log after Params"
+sleep 10
 
 
 ###################################################################################################
@@ -82,36 +85,49 @@ function Join-Domain
         [securestring] $Password,
         [string] $DomainServer
     )
+    	sleep 10
 	Add-Content -Path $file -Value "Entering Join-Domain Function, logging after Params: $DomainName,$OUPath,$User,$Password,$DomainServer"
 	
     if ((Get-WmiObject Win32_ComputerSystem).Domain -eq $DomainName)
     {
+    	sleep 10
         Add-Content -Path $file -Value "Server already Joined to domain!"
+	sleep 5
         Write-Host "Computer $($Env:COMPUTERNAME) is already joined to domain $DomainName."
     }
     else
     {
+    	sleep 10
     	Add-Content -Path $file -Value "Entering First ELSE clause: $DomainName,$User,$Password,$DomainServer"
+	sleep 5
         $credential = New-Object System.Management.Automation.PSCredential($User, $Password)
         
         if ($OUPath)
         {
+	    sleep 10
 	    Add-Content -Path $file -Value "Entering IF when OU is provided: $DomainName,$OUPath,$User,$Password,$DomainServer"
+	    sleep 5
             [Microsoft.PowerShell.Commands.ComputerChangeInfo]$computerChangeInfo = Add-Computer -DomainName $DomainName -OUPath $OUPath -Credential $credential -Server $DomainServerJoin -Force -PassThru
         }
         else
         {
+	    sleep 10
 	    Add-Content -Path $file -Value "Entering IF when OU is NOT provided: $DomainName,$User,$Password,$DomainServer"
+	    sleep 5
             [Microsoft.PowerShell.Commands.ComputerChangeInfo]$computerChangeInfo = Add-Computer -DomainName $DomainName -Credential $credential -Server $DomainServerJoin -Force -PassThru
         }
         
         if (-not $computerChangeInfo.HasSucceeded)
         {
             throw "Failed to join computer $($Env:COMPUTERNAME) to domain $DomainName."
+	    sleep 5
 	    write-host "Computer $($Env:COMPUTERNAME) failed to joined domain $DomainName."
+	    sleep 10
 	    Add-Content -Path $file -Value "Logging if AD Join Failed - $DomainName,$OUPath,$User,$Password,$DomainServer"
         }
+	sleep 10
         Add-Content -Path $file -Value "Logging if AD Join was Successful - $DomainName,$OUPath,$User,$Password,$DomainServer"
+	sleep 5
         write-host "Computer $($Env:COMPUTERNAME) successfully joined domain $DomainName."
     }
 }
@@ -129,11 +145,15 @@ try
     }
 
     write-host "Attempting to join computer $($Env:COMPUTERNAME) to domain $DomainToJoin."
+    sleep 10
     Add-Content -Path $file -Value "Entering Main Script Block (try) function - $DomainToJoin,$OUPath,$DomainAdminUsername,$DomainAdminPassword,$DomainServerJoin"
+    sleep 5
     $securePass = ConvertTo-SecureString "$DomainAdminPassword" -AsPlainText -Force
+    sleep 10
     Join-Domain -DomainName $DomainToJoin -OUPath "$OUPath" -User $DomainAdminUsername -Password $securePass -DomainServer $DomainServerJoin 
-
+    sleep 10
     Add-Content -Path $file -Value "Entering Main Script Block (try) - Artifact Applied Successfully"
+    sleep 5
     Write-Host 'Artifact applied successfully.'
 }
 finally
